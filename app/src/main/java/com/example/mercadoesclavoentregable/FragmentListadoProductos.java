6e8881380ddd38b79666c1676664d9745515d110
Theo <theomusumeci@gmail.com>
2020-05-01 20:23:48 -0300

    Primer Sprint

--- app/src/main/java/com/example/mercadoesclavoentregable/FragmentListadoProductos.java
+++ app/src/main/java/com/example/mercadoesclavoentregable/FragmentListadoProductos.java
@@ -3,62 +3,42 @@ package com.example.mercadoesclavoentregable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentListadoProductos#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FragmentListadoProductos extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public FragmentListadoProductos() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentListadoProductos.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentListadoProductos newInstance(String param1, String param2) {
-        FragmentListadoProductos fragment = new FragmentListadoProductos();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View fragmentInflado = inflater.inflate(R.layout.fragment_listado_productos, container, false);
+
+
+        RecyclerView recyclerViewProductos = fragmentInflado.findViewById(R.id.fragmentListadoRecyclerView);
+
+        List<Producto> listaDeProductos = ProveedorDeProductos.getProducto();
+        ProductoAdapter productoAdapter = new ProductoAdapter(listaDeProductos);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(fragmentInflado.getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerViewProductos.setLayoutManager(linearLayoutManager);
+
+
+        recyclerViewProductos.setAdapter(productoAdapter);
+        return fragmentInflado;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listado_productos, container, false);
-    }
+
 }
