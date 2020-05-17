905445b438ac227ea661a48829d6e89128a6c9c1
Theo <theomusumeci@gmail.com>
2020-05-17 16:56:42 -0300

    Agrego NavigationView con head

---
+++ app/src/main/java/com/example/mercadoesclavoentregable/FragmentDetails.java
@@ -0,0 +1,28 @@
+package com.example.mercadoesclavoentregable;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentDetails extends Fragment {
+
+    public FragmentDetails() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/mercadoesclavoentregable/view/ProductoAdapter.java
@@ -0,0 +1,68 @@
+package com.example.mercadoesclavoentregable.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mercadoesclavoentregable.R;
+import com.example.mercadoesclavoentregable.model.Producto;
+
+import java.util.List;
+
+public class ProductoAdapter extends RecyclerView.Adapter<ProductoAdapter.ProductoViewHolder> {
+
+
+    private List<Producto> listaDeProductos;
+    //TODO crear el listener
+    //private ProductosAdapterListener listener;
+
+    public ProductoAdapter(List<Producto> listaDeProductos) {
+        this.listaDeProductos = listaDeProductos;
+    }
+
+    @NonNull
+    @Override
+    public ProductoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater infladorLayout = LayoutInflater.from(parent.getContext());
+        View celdaView = infladorLayout.inflate(R.layout.celda_producto, parent, false);
+        ;
+        return new ProductoViewHolder(celdaView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductoViewHolder holder, int position) {
+        Producto productoAMostrar = listaDeProductos.get(position);
+        holder.cargarProducto(productoAMostrar);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.listaDeProductos.size();
+    }
+
+    protected class ProductoViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView nombreProducto;
+        private TextView precioProducto;
+        private ImageView fotoProducto;
+
+
+        public ProductoViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nombreProducto = itemView.findViewById(R.id.celdaArticuloTextViewArticulo);
+            precioProducto = itemView.findViewById(R.id.celdaArticuloTextViewPrecio);
+            fotoProducto = itemView.findViewById(R.id.celdaArticuloImageViewFoto);
+
+        }
+
+        public void cargarProducto(Producto unProducto) {
+            nombreProducto.setText(unProducto.getNombreProducto());
+            precioProducto.setText(unProducto.getPrecioProducto().t
